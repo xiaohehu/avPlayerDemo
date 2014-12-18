9ad15bb15ff7d9bb942c18190230787e3dfb485c
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-12-18 09:13:31 -0500

    Initial Commit

---
+++ AvPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AvPlayerDemo
+//
+//  Created by Xiaohe Hu on 12/18/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AvPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvPlayerDemo
+//
+//  Created by Xiaohe Hu on 12/18/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
